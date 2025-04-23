673a9b3a0a8a3fc6f50459bf2cb707136b7db7ee
Luffy <luffydbl@outlook.com>
2025-04-23 15:12:53 +0800

    fix name  miss adhocuwb_

--- platform/Inc/adhocuwb_platform.h
+++ platform/Inc/adhocuwb_platform.h
@@ -4,15 +4,15 @@
 #define CONFIG_ADHOCUWB_PLATFORM_CRAZYFLIE y
 
 #if defined(CONFIG_ADHOCUWB_PLATFORM_CRAZYFLIE)
-    #include "platform_crazyflie.h"
+    #include "adhocuwb_platform_crazyflie.h"
 #elif defined(CONFIG_ADHOCUWB_PLATFORM_ATHENA)
-    #include "platform_athena3.2.h"
+    #include "adhocuwb_platform_athena3.2.h"
 #elif defined(CONFIG_ADHOCUWB_PLATFORM_ADHOCUWB)
-    #include "platform_adhocuwb.h"
+    #include "adhocuwb_platform_adhocuwb.h"
 #elif defined(CONFIG_ADHOCUWB_PLATFORM_LINUX)
-    #include "platform_linux.h"
+    #include "adhocuwb_platform_linux.h"
 #elif defined(CONFIG_ADHOCUWB_PLATFORM_MACOS)
-    #include "platform_macos.h"
+    #include "adhocuwb_platform_macos.h"
 #else
     //
 #endif
